450b0061e9f813ef6fb5e9c94d84d1741c7bebcd
Max Nguyen <max.nguyen2411@gmail.com>
2020-02-10 13:21:32 +0700

    Last updated 4 New Year 2020

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hi Everybody, welcome to my blog :) !!!');
+  res.send('Hi Everybody, welcome to my blog :) !!! [Updated on 10/02/2020]');
 });
 
 app.listen(process.env.PORT || 5000);
