1711dae993b4acc96c32f11db36500ba6ca4ba14
Max Nguyen <max.nguyen2411@gmail.com>
2020-02-26 10:10:41 +0700

    test 4 jenkins update on OS

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hi Everybody, welcome to my blog :) !!! [Updated on 25/02/2020 17:00]');
+  res.send('Hi Everybody, welcome to my blog :) !!! [Updated on 26/02/2020 17:00]');
 });
 
 app.listen(process.env.PORT || 5000);
